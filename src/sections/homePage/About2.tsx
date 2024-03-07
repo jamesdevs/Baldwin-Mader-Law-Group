820cb237786b0a17079b0da1edc885cfdda54aae
AmirSoftwareDeveloper <seyed.taba.nej@gmail.com>
2024-03-07 10:03:20 -0800

    update about image

--- src/sections/homePage/About2.tsx
+++ src/sections/homePage/About2.tsx
@@ -57,7 +57,7 @@ const About2 = () => {
           <div className="flex w-full relative mx-auto max-w-xl min-h-[250px] sm:min-h-[350px] order-1 lg:order-2">
             <div className="flex w-full whitespace-nowrap relative">
               <Image
-                src="/images/hero/image-17.jpg"
+                src="/images/hero/image-22.jpg"
                 alt="about"
                 className="h-full w-full min-h-[200px]"
                 imageClassName="object-cover"
